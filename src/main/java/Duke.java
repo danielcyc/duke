9e58b0c4b7273839bfde6738a294254e5216347c
Daniel <daniel.c.y.cyc@gmail.com>
2019-08-27 03:43:46 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,23 +6,36 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
         String line = "___________________________________________________________________\n";
         String space = "    ";
 
         System.out.println("Hello from\n" + logo);
-
         System.out.println(space + line + space + "Hello! I'm Duke\n" +
                         space + "What can I do for you?\n"+
                             space + line);
+
+        String[] userlist = new String[100];
+        int counter = 0;
+
         Scanner user_input = new Scanner(System.in);
         while (true) {
-            String input = user_input.nextLine();
+            String input = user_input.nextLine(); // take in input
             if (input.equals("bye")) {
-                System.out.println("\n" + space + line + space + "Bye. Hope to see you again soon!" + "\n" + space + line);
+                System.out.println( space + line + space + "Bye. Hope to see you again soon!" + "\n" + space + line);
                 break;
+            } else if (input.equals("list")) {
+
+                System.out.print(space + line);
+                for (int i = 0; i < counter; i++){
+                    System.out.print(space + " " + (i+1) + ". "+ userlist[i] + "\n");
+                }
+                System.out.print(space + line);
+                //print out counter and list
+
             } else {
-                System.out.println("\n" + space + line + space + input + "\n" + space + line);
+                userlist[counter] = input;
+                counter += 1;
+                System.out.println(space + line + space + " added: " + input + "\n" + space + line);
             }
         }
 
