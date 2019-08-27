a70eb60da38041fd4ebaf2aaad9e69dc0996c062
Daniel <daniel.c.y.cyc@gmail.com>
2019-08-27 18:51:21 +0800

    level 3 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,28 +14,40 @@ public class Duke {
                         space + "What can I do for you?\n"+
                             space + line);
 
-        String[] userlist = new String[100];
+        Task[] userlist = new Task[100];
         int counter = 0;
 
         Scanner user_input = new Scanner(System.in);
-        while (true) {
+        while (true) { // keep program running
             String input = user_input.nextLine(); // take in input
             if (input.equals("bye")) {
                 System.out.println( space + line + space + "Bye. Hope to see you again soon!" + "\n" + space + line);
                 break;
-            } else if (input.equals("list")) {
+            }
+            else if (input.equals("list")) {
 
-                System.out.print(space + line);
+                System.out.print(space + line + space +"Here are the tasks in your list: \n");
                 for (int i = 0; i < counter; i++){
-                    System.out.print(space + " " + (i+1) + ". "+ userlist[i] + "\n");
+                    System.out.print(space + (i+1) + ".["+ userlist[i].getStatusIcon() + "] " + userlist[i].getDescription() + "\n");
                 }
-                System.out.print(space + line);
+                System.out.println(space + line);
                 //print out counter and list
 
-            } else {
-                userlist[counter] = input;
+            }
+            else if (input.startsWith("done")) {
+                String[] parts = input.split(" ");
+                int index = Integer.parseInt(parts[1]) -1 ;
+                userlist[index].markAsDone();
+                System.out.print(space + line);
+                System.out.println(space + "Nice! I've marked this task as done:\n" + space + "  [" + userlist[index].getStatusIcon() + "] " + userlist[index].getDescription());
+                System.out.print(space + line);
+
+            }else
+             {
+                 Task item = new Task(input);
+                 userlist[counter] = item;
                 counter += 1;
-                System.out.println(space + line + space + " added: " + input + "\n" + space + line);
+                System.out.println(space + line + space + " added: " + item.getDescription() + "\n" + space + line);
             }
         }
 
